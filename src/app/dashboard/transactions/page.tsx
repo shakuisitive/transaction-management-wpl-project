70b03ceccb389dc1a1d379f3b9b790b276b7c8d6
Shakir <shakirkhan72727@gmail.com>
2025-06-01 22:02:02 +0500

    show data in a shadcn table component

--- src/app/dashboard/transactions/page.tsx
+++ src/app/dashboard/transactions/page.tsx
@@ -8,9 +8,12 @@ import {
   } from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { getTransactionsByMonth } from "@/data/getTransactionsByMonth";
 import { format } from "date-fns";
+import { PencilIcon } from "lucide-react";
 import Link from "next/link";
+import numeral from 'numeral'
 
 import {z} from 'zod';
 let today = new Date();
@@ -33,8 +36,6 @@ async function TransactionsPage({searchParams}: {
   let selectedDate = new Date(year, month - 1, 1);
 
   let transactions = await getTransactionsByMonth({month, year});
-
-  console.log(transactions)
 
   return (
     <div className="max-w-screen-xl mx-auto py-10">
@@ -68,6 +69,32 @@ async function TransactionsPage({searchParams}: {
           <Button asChild>
             <Link href="/dashboard/transactions/new">New Transaction</Link>
           </Button>
+
+          {!transactions?.length && <p className="text-center py-10 text-lg text-muted-foreground">There is no transaction for this month.</p>}
+        
+          {!!transactions?.length && <Table className="mt-4">
+            <TableHeader>
+              <TableRow>
+                <TableHead>Date</TableHead>
+                <TableHead>Description</TableHead>
+                <TableHead>Type</TableHead>
+                <TableHead>Category</TableHead>
+                <TableHead>Amount</TableHead>
+                <TableHead/>
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              {transactions.map(transaction => (<TableRow key={transaction.id}>
+                  <TableCell>{format(transaction.transactionDate, 'do MM yyyy')}</TableCell>
+                  <TableCell>{transaction.description}</TableCell>
+                  <TableCell>{transaction.categoryId}</TableCell>
+                  <TableCell>{transaction.categoryId}</TableCell>
+                  <TableCell>${numeral(transaction.amount).format('0,0[.]00')}</TableCell>
+                  <TableCell className="text-right"><Button aria-label="Edit transaction" asChild size="icon" variant="outline"><Link href={`/dashboard/transactions/${transaction.id}`}><PencilIcon/></Link></Button></TableCell>
+                </TableRow>))}
+            </TableBody>
+            </Table>}
         </CardContent>
       </Card>
     </div>
